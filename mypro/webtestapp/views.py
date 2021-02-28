4e4b4104bafd98f8ffd1cef9d8c2233563d07533
Glitch (python-django-example) <none>
2021-02-28 08:35:40 +0000

    🌃🌼 Checkpoint ./mypro/webtestapp/views.py:18728712/388

--- mypro/webtestapp/views.py
+++ mypro/webtestapp/views.py
@@ -2,6 +2,8 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import TestForm # forms.pyに記述したTestFormをimport
+
 
 # def index(request):
 #     return HttpResponse("Hello World fuck!")
@@ -9,8 +11,13 @@ from django.http import HttpResponse
 def index(request):
     my_dict = {
       'insert_something':"views.pyのfucking insert_something部分です" ,
-      'name':"morioka"
+      'name':"morioka" ,
+      'form':TestForm() ,
+      'insert_forms':'default_value'
     }
+    if (request.method == 'POST'):
+      my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '<br>整数型:' + request.POST['num']
+      my_dict['form'] = TestForm(request.POST)
     return render(request,'webtestapp/index.html' , my_dict)
  
  
