d752316319f24145ac7128eba65b1e394ad5dc69
Glitch (python-django-example) <none>
2021-03-11 03:45:31 +0000

    🎀🍛 Checkpoint ./mypro/webtestapp/models.py:18728712/1391

--- mypro/webtestapp/models.py
+++ mypro/webtestapp/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 class InfoModelForm(models.Model):
     name = models.CharField('名前',max_length=255)
     mail = models.EmailField('メール',max_length=255)
-    gender = models.IntegerField('性別')
+    gender = models.BooleanField('性別')
     department = models.CharField('部署',max_length=255)
     year = models.IntegerField('社歴',default=0)
     created_at = models.DateField('作成日',default=timezone.now)
@@ -16,4 +16,14 @@ class InfoModelForm(models.Model):
     def __str__(self):
         return '<id:' + str(self.id) + ',' + self.name + ',' + self.department + '>'
 
+class SearchModelForm(models.Model):
+    name = models.CharField('名前',max_length=255)
+    mail = models.EmailField('メール',max_length=255)
+    gender = models.CharField('性別')
+    department = models.CharField('部署',max_length=255)
+    year = models.CharField('社歴',default=0)
+    created_at = models.DateField('作成日',default=timezone.now)
+
+    def __str__(self):
+        return '<id:' + str(self.id) + ',' + self.name + ',' + self.department + '>'      
 
